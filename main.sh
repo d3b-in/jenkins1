0ec1649f2d98bfbd2c2a307fe351790f9dfc2ac4
Munendra Kumar <148196596+d3b-in@users.noreply.github.com>
2023-10-20 11:48:54 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,5 +3,5 @@ echo "Build Stage 2"  >> build.log
 echo "Build Stage 2"  >> build.log
 echo "Build Finished" >> build.log
 echo "Uptime is $(uptime)"
-
+echo "Hostname if $(hostname -f)"
 echo "Finished at $(date)"
