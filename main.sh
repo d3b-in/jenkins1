00e52dfed0f2e8d6a4a21e9e05759efb5e46e946
Munendra Kumar <148196596+d3b-in@users.noreply.github.com>
2023-10-20 11:48:28 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,6 @@ echo "Build Stage 1"  >> build.log
 echo "Build Stage 2"  >> build.log
 echo "Build Stage 2"  >> build.log
 echo "Build Finished" >> build.log
+echo "Uptime is $(uptime)"
+
 echo "Finished at $(date)"
